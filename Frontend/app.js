4f646b481d74ffac103dcd6b31776751e769e645
Zheng Roger Luo <ZhengRogerLuo@gmail.com>
2022-07-14 16:40:33 +1000

    14/July update 5: Implemented the structure(EventListener) for volume up.

---
+++ Frontend/app.js
@@ -0,0 +1,7 @@
+const volumeUpButton = document.getElementById("volumeUpButton");
+
+volumeUpButton.addEventListener("click", volumeIncrement);
+
+function volumeIncrement() {
+  console.log("VOLUME UP!");
+}
