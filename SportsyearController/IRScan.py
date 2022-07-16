e727f231293793ca448c86c0ad01ff5e51fd0a65
kevin hu <kevinhu12@hotmail.com>
2022-07-17 02:25:16 +1000

    testing IRScan given a number of devices

--- SportsyearController/IRScan.py
+++ SportsyearController/IRScan.py
@@ -4,22 +4,35 @@ from urllib.parse import unquote
 from time import sleep
 from flask import Blueprint
 
-def IRScan():
-    request = b'M-SEARCH * HTTP/1.1\r\n' \
-              b'HOST: 239.255.255.250:1900\r\n' \
-              b'MAN: "ssdp:discover"\r\n' \
-              b'MX: 10\r\n' \
-              b'ST: ssdp:all\r\n\r\n'
-
+def IRScan(expected_number_devices):
+    SSDP_DISCOVER = ('M-SEARCH * HTTP/1.1\r\n' +
+                     'HOST: 239.255.255.250:1900\r\n' +
+                     'MAN: "ssdp:discover"\r\n' +
+                     'MX: 10\r\n' +
+                     'ST: ssdp:all\r\n\r\n')
+    BCAST_IP = '239.255.255.250'
+    BCAST_PORT = 1900
+    LINES_IN_RESPONSE = 7
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(request, (b'239.255.255.250', 1900))
     sock.settimeout(10)
-
-    while True:
-        response, address = sock.recvfrom(512)
-        print(response.decode())
-        print()
-
+    found_devices = 0
+    devices = []
+    found_devices_list = []
+    while found_devices < expected_number_devices:
+        sock.sendto(SSDP_DISCOVER.encode('ASCII'), (BCAST_IP, BCAST_PORT))
+        response, _ = sock.recvfrom(1024)
+        response = response.decode().split('\n')
+        if len(response) == LINES_IN_RESPONSE:
+            if "arduino" in response[3].lower():
+                address = response[6].split(' ')[1].split('/')[2]
+                if address not in found_devices_list:
+                    tv_name = response[3].split(' ')[4][:-1]
+                    blaster_hostname = address.split(':')[0]
+                    blaster_port = address.split(':')[1]
+                    devices.append((tv_name,blaster_hostname,blaster_port))
+                    found_devices_list.append(address)
+                    found_devices += 1
     sock.close()
+    return devices
 
-IRScan()
+print(IRScan(1))
