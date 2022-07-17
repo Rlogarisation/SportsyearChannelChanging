b15a2d5a429d8740890925c0c13cc80c92906d1a
kevin hu <kevinhu12@hotmail.com>
2022-07-17 18:07:48 +1000

    including routes for IR solution

--- backend/routes/IR.py
+++ backend/routes/IR.py
@@ -2,10 +2,10 @@ from werkzeug.exceptions import BadRequest
 from flask import Blueprint, request
 from json import requests
 
-IR = Blueprint('IR', __name__, url_prefix='/IR/')
+IR = Blueprint('IR', __name__, url_prefix='/ir/')
 
 """
-Send GET request to IR/test_ir
+Send GET request to ir/test_ir
 Body must include:
 IP = ip number of arduino device
 PORT = port number of arduino device
@@ -33,29 +33,203 @@ def test_ir():
     }
 
 """
-Send POST request to IR/test_ir
+Send POST request to ir/set_channel
 Body must include:
 IP = ip number of arduino device
 PORT = port number of arduino device
-code = hexadecimal code or an array of int
-passcode = passcode for sending requests
+CHANNEL = channel to change to
+TYPE = tv type
+LENGTH = length of code to be sent
 """
-@IR.route("/test_post_with_body")
+@IR.route("/set_channel")
 def test_post_with_body():
     data = request.get_json()
     IP = data['IP']
     PORT = data['PORT']
+    CHANNEL = data['CHANNEL']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
 
-    # IR code such as A90:SONY:12
-    code = data['code']
-    # Passcode required to execute IR command
-    passcode = data['passcode']
-
-    URL = f'http://{IP}:{PORT}/msg?pass={passcode}'
+    URL = f'http://{IP}:{PORT}/ir/set_channel?channel={CHANNEL}&type={TYPE}&length={LENGTH}'
     myobj = {
-        "type":"lg",
-        "data": code,
-        "length":32 # bit length
+        "channel" : data['CHANNEL'],
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+
+"""
+Send POST request to ir/lower_volume
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/lower_volume")
+def lower_volume():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/lower_volume?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+"""
+Send POST request to ir/raise_volume
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/raise_volume")
+def raise_volume():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/raise_volume?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+"""
+Send POST request to ir/mute
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/mute")
+def mute():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/mute?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+"""
+Send POST request to ir/power
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/power")
+def power():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/power?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+"""
+Send POST request to ir/raise_channel
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/raise_channel")
+def raise_channel():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/raise_channel?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
+    }
+
+    r = requests.post(URL, myobj)
+    data = r.json()
+    print(data)
+    return {
+        'data' : data
+    }
+
+"""
+Send POST request to ir/lower_channel
+Body must include:
+IP = ip number of arduino device
+PORT = port number of arduino device
+TYPE = tv type
+LENGTH = length of code to be sent
+"""
+@IR.route("/lower_channel")
+def lower_channel():
+    data = request.get_json()
+    IP = data['IP']
+    PORT = data['PORT']
+    TYPE = data['TYPE']
+    LENGTH = data['LENGTH']
+
+    URL = f'http://{IP}:{PORT}/ir/lower_channel?type={TYPE}&length={LENGTH}'
+    myobj = {
+        "type" : data['TYPE'],
+        "length" : data['LENGTH'],
     }
 
     r = requests.post(URL, myobj)
