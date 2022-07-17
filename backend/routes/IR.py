5680d2d4495cd6d6fb7adb7b7d09a6d749946a83
AnonymousAlligator <kevinhu12@hotmail.com>
2022-07-17 19:04:56 +1000

    WIP IRScan route

--- backend/routes/IR.py
+++ backend/routes/IR.py
@@ -1,8 +1,40 @@
 from werkzeug.exceptions import BadRequest
 from flask import Blueprint, request
 import requests
+import socket
 
 IR = Blueprint('IR', __name__, url_prefix='/ir/')
+
+def IRScan():
+    SSDP_DISCOVER = ('M-SEARCH * HTTP/1.1\r\n' +
+                        'HOST: 239.255.255.250:1900\r\n' +
+                        'MAN: "ssdp:discover"\r\n' +
+                        'MX: 10\r\n' +
+                        'ST: upnp:rootdevice\r\n\r\n')
+    BCAST_IP = '239.255.255.250'
+    BCAST_PORT = 1900
+    LINES_IN_RESPONSE = 9
+    devices = []
+    found_devices_list = []
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(10)
+        sock.sendto(SSDP_DISCOVER.encode('ASCII'), (BCAST_IP, BCAST_PORT))
+        while True:
+            response, _ = sock.recvfrom(1024)
+            response = response.decode().split('\n')
+            if len(response) == LINES_IN_RESPONSE:
+                if "arduino" in response[3].lower():
+                    address = response[6].split(' ')[1].split('/')[2]
+                    if address not in found_devices_list:
+                        tv_name = response[3].split(' ')[4].split('/')[0]
+                        blaster_hostname = address.split(':')[0]
+                        blaster_port = address.split(':')[1]
+                        devices.append((tv_name,blaster_hostname,blaster_port))
+                        found_devices_list.append(address)
+    except:
+        sock.close()
+        return devices
 
 """
 Send GET request to ir/test_ir
@@ -12,19 +44,9 @@ PORT = port number of arduino device
 code = hexadecimal code or an array of int
 passcode = passcode for sending requests
 """
-@IR.route("/test_ir", methods=['GET'])
+@IR.route("/scan", methods=['GET'])
 def test_ir():
-    data = request.get_json()
-    IP = data['IP']
-    PORT = data['PORT']
-
-    # IR code such as A90:SONY:12
-    code = data['code']
-    # Passcode required to execute IR command
-    passcode = data['passcode']
-
-    URL = f'http://{IP}:{PORT}/msg?code={code}&pass={passcode}'
-    # r = requests.post(url = URL)
+    ir_device_list = IRScan()
     r = requests.get(url = URL)
     data = r.json()
     print(data)
