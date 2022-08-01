85dd1347ce62a314bb7ed6c77ca64495c3c013c0
z5207116 <41610660+z5207116@users.noreply.github.com>
2022-08-02 02:58:14 +1000

    Added pause and resume routes for IR

--- backend/routes/automation.py
+++ backend/routes/automation.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.combining import AndTrigger
 from apscheduler.triggers.interval import IntervalTrigger
-from channel_checking import obtain_schedule, channel_automation
+from channel_checking import obtain_schedule, channel_automation, channel_automation_IR
 
 def initialiseAutomation():
     # Background task scheduler
@@ -13,15 +13,17 @@ def initialiseAutomation():
 
     trigger1 = AndTrigger([IntervalTrigger(hours=24)])
     trigger2 = AndTrigger([IntervalTrigger(minutes=.1)])
-    global scheduled_job, scheduled_job2
+    trigger3 = AndTrigger([IntervalTrigger(minutes=.1)])
+    global scheduled_job, scheduled_job2, scheduled_job3
     scheduled_job = scheduler.add_job(obtain_schedule,trigger1)
     scheduled_job2 = scheduler.add_job(channel_automation,trigger2)
-    scheduled_job2.pause()
+    scheduled_job3 = scheduler.add_job(channel_automation_IR,trigger3)
+    #scheduled_job2.pause()
 
 automation = Blueprint('automation', __name__, url_prefix='/automation/')
 
 """
-Pause Automation
+Pause Automation Wifi
 Method = POST
 """
 @automation.route("/pause", methods=['POST'])
@@ -30,10 +32,29 @@ def pause():
     return {}
 
 """
-Resume Automation
+Resume Automation Wifi
 Method = POST
 """
 @automation.route("/resume", methods=['POST'])
 def resume():
     scheduled_job2.resume()
     return {}
+
+"""
+Pause Automation IR
+Method = POST
+"""
+@automation.route("/pause_IR", methods=['POST'])
+def pause_IR():
+    scheduled_job3.pause()
+    return {}
+
+"""
+Resume Automation IR
+Method = POST
+"""
+@automation.route("/resume_IR", methods=['POST'])
+def resume_IR():
+    scheduled_job3.resume()
+    return {}
+
